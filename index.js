aa83b07a40c4d72e409234f8d67df1abcdb829ab
oncomouse <oncomouse@gmail.com>
2020-07-27 19:04:44 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['kefir'], factory); // AMD
+  } else if (typeof exports === 'object') {
+    factory(require('kefir')); // CommonJS
+  } else {
+    factory(root.Kefir); // Browser globals
+  }
+})(typeof self !== 'undefined' ? self : this, function (Kefir) {
+  Kefir.$ = {};
+  Kefir.$.init = function ($) {
+    $.fn.asKefirStream = function (eventName, selector, transformer) {
+      var $el = this;
+      if (transformer === null && selector !== null && 'string' !== typeof selector) {
+        transformer = selector;
+        selector = null;
+      }
+      return Kefir.fromSubUnsub(
+        function (handler) {$el.on(eventName, selector, handler)},
+        function (handler) {$el.off(eventName, selector, handler)},
+        transformer
+      ).setName('asKefirStream');
+    }
+  };
+});
